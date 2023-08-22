a9b06a1714916edec9fddc2c280783de89380828
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-08-22 17:57:40 +0300

    status

--- login/index.php
+++ login/index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <tit../title>
+    <tit.. /title>
 </head>
 
 <body>
-    naber
+    naberr
 </body>
 
 </html>
